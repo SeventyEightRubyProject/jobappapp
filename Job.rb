16d3f3ba973df0ae6254413344f44c1cd724a176
Cloudfactory Workstation <cloudfactory@Cloudfactory-Workstations-iMac-2.local>
2012-12-03 08:59:15 +0545

    Re-Add Job and applicant classes

--- Job.rb
+++ Job.rb
@@ -1,1 +1,13 @@
-class 
+class Job
+	attr_accessor :id, :title
+	@@job_counter = 0
+	@@joblist = [["0","No title"]]
+
+	def initialize(title)
+		@@job_counter += 1
+		id = job_counter
+		@title = title
+		@@joblist << [[id,title]]
+	end
+
+end
