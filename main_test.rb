aa1c0910e1e4f0e57deb8205c509350d045cd00a
Barun Thapa <barunthapa.bvdt@gmail.com>
2012-12-03 15:09:17 +0545

    Add Display Methods

--- main_test.rb
+++ main_test.rb
@@ -16,7 +16,7 @@ class Applicant
 	end
 
 	def applied_jobs
-		puts "Job id: \t Job title: "	
+		puts "Job id: \t Job title: \t requested by:"	
 		@joblist.each do |jobs|
 			puts "#{jobs.id} \t\t #{jobs.title}"		
 		end
@@ -33,18 +33,23 @@ class Job
 	
 
 	def self.show_jobs
-		puts @@joblist
+		puts "Job id: \t Job title: \t\t Requested by:"
+
+		@@joblist.each do |job|
+			puts "#{job.id} \t\t #{job.title} \t\t\t #{job.requestor.name}"
+		end
 	end
 
 	def initialize(title)
 		@@job_counter += 1
 		@id = @@job_counter
 		@title = title
-		@@joblist = @@joblist.push([id,title])
+		@@joblist = @@joblist.push(self)
 		@applicant_list = []
 	end
 
 	def showapplicants
+			puts "Applicant List for #{self.title}"
 		@applicant_list.each do |applicant|
 			puts applicant.name
 		end
@@ -85,29 +90,44 @@ end
 
 #create a new job
 
-job = Job.new("Junior Assistant Engineer")
+
 re = Requester.new(1,"Barun")
+req2 = Requester.new(2,"bhanu")
+
+job = Job.new("Junior Engineer")
+job2 = Job.new("developer")
+job3 = Job.new("QA")
+
 re.create_job(job)
+re.create_job(job2)
+req2.create_job(job3)
+
 applicant = Applicant.new("RDM")
+amit = Applicant.new("Amit s")
+
 applicant.apply(job)
+applicant.apply(job3)
+amit.apply(job2)
+amit.apply(job3)
+
+puts "All job lists"
+Job.show_jobs
+puts "\n requester"
 job.showrequester
 
 #job.applicants
-
-job2 = Job.new("developer")
-applicant.apply(job2)
+puts "\nAllpied jobs by applicant 1"
 applicant.applied_jobs
 
-Job.show_jobs
 
-amit = Applicant.new("Amit s")
-amit.apply(job2)
+
+
+
+
 puts "amit ko job"
 amit.applied_jobs
 
-job3 = Job.new("bhada majhne")
-applicant.apply(job3)
 applicant.applied_jobs
 
-Job.show_jobs
-job2.showapplicants
+puts"\nApplicants for job 2"
+job3.showapplicants
